9abb44d6574728047c3ffbe99587fc22ea75ce30
Nicolas Lujan <nico512lujan@gmail.com>
2023-02-08 11:31:30 -0300

    Last changes just for push

--- app.js
+++ app.js
@@ -12,7 +12,15 @@ app.use(express.json())
 app.use(logger)
 app.use(express.static('public'))
 
-// ----------Gets----------
+
+// ----------Routers----------
+
+const routerArgentina = express.Router()
+app.use('/api/argentina/', routerArgentina);
+
+
+// ---------- Gets ----------
+
 app.get('/', (req, res) => {
 	res.redirect('/index.html')
 })
@@ -21,17 +29,59 @@ app.get('/api', (req, res) => {
 	res.redirect('../index.html')
 })
 
-app.get('/api/argentina', (req, res) => {
+// --- Router Arg --
+
+routerArgentina.get('/', (req, res) => {
 	res.json(argentinianTeam)
 })
 
+// Get segun posicion
+
+routerArgentina.get('/arqueros', (req, res) => {
+	const arqueros = argentinianTeam.filter(jugador => jugador.position == 'Arquero')
+	res.json(arqueros)
+})
+
+routerArgentina.get('/delanteros', (req, res) => {
+	const delantero = argentinianTeam.filter(jugador => jugador.position == 'Delantero')
+	res.json(delantero)
+})
+
+routerArgentina.get('/mediocampistas', (req, res) => {
+	const mediocampistas = argentinianTeam.filter(jugador => jugador.position == 'Mediocampista')
+	res.json(mediocampistas)
+})
+
+
+routerArgentina.get('/defensores', (req, res) => {
+	const defensores = argentinianTeam.filter(jugador => jugador.position == 'Defensor')
+	res.json(defensores)
+})
+
+// Get segun dorsal o nombre
+
+routerArgentina.get('/:dorsalOrName', (req, res) => {
+	if (!isNaN(req.params.dorsalOrName)) {
+		const jugadorDorsal = argentinianTeam.filter(element => element.dorsal == req.params.dorsalOrName)
+		res.json(jugadorDorsal)
+	} else {
+		const jugadorName = argentinianTeam.filter(jugador =>
+			jugador.name.split(' ')[1].toUpperCase() == req.params.dorsalOrName.toUpperCase() ||
+			jugador.name.split(' ')[0].toUpperCase() == req.params.dorsalOrName.toUpperCase()
+		)
+		res.json(jugadorName)
+	}
+})
+
+
 // ----------POST----------
 
-app.post('/api/argentina', (req, res) => {
+routerArgentina.post('/', (req, res) => {
 	const newPlayer = req.body;
 	argentinianTeam.push(newPlayer);
 	res.json(newPlayer); 
 })
+
 
 // ----------Manejar error----------
 app.use((req, res) => {
